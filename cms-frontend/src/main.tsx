96c902aa0d42a4c1a96745661c15b06cd559379f
Vishesh <visheshdpanchal@gmail.com>
2024-03-16 20:10:19 +0530

    feat:setup router and shadcn

--- cms-frontend/src/main.tsx
+++ cms-frontend/src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { CookiesProvider } from "react-cookie";
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+// Create a new router instance
+const router = createRouter({
+  routeTree,
+});
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <CookiesProvider>
+          <RouterProvider router={router} />
+      </CookiesProvider>
+    </StrictMode>,
+  );
+}
---
+++ cms-frontend/src/routes/__root.tsx
@@ -0,0 +1,23 @@
+import {
+    createRootRouteWithContext,
+    Outlet,
+  } from "@tanstack/react-router";
+  import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+  import { Toaster } from "react-hot-toast";
+
+
+
+  const RootComponent = () => {
+    return (
+      <div>
+        <Toaster />
+        <Outlet />
+        <TanStackRouterDevtools />
+      </div>
+    );
+  };
+  export const Route = createRootRouteWithContext()({
+    component: () => (
+          <RootComponent />
+    ),
+  });
